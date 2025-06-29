1b16dc6f8a02629bfa93023faaed9d86d9e0125e
Langenheim Geronimo Valentin <langenheimgeronimo8@gmail.com>
2025-06-29 16:25:29 -0300

    haciendo dinamica las cartas del menu

--- js/compra.js
+++ js/compra.js
@@ -1,3 +1,85 @@
+/* LOGICA DE CREACION DE CARTAS */
+const menu = [
+  {
+    titulo: "Hamburguesas",
+    productos: [
+      {img: "/img/hamburguesa.png", nombre: "triple con queso", precio: 5500, descripcion: "Tres medallones de carne 100% vacuna, queso derretido, mostaza, kétchup y cebolla triturada, es algo que nunca puede fallar"},
+      {img: "/img/hamburguesa.png", nombre: "triple con queso", precio: 5500, descripcion: "Tres medallones de carne 100% vacuna, queso derretido, mostaza, kétchup y cebolla triturada, es algo que nunca puede fallar"},
+      {img: "/img/hamburguesa.png", nombre: "triple con queso", precio: 5500, descripcion: "Tres medallones de carne 100% vacuna, queso derretido, mostaza, kétchup y cebolla triturada, es algo que nunca puede fallar"},
+      {img: "/img/hamburguesa.png", nombre: "triple con queso", precio: 5500, descripcion: "Tres medallones de carne 100% vacuna, queso derretido, mostaza, kétchup y cebolla triturada, es algo que nunca puede fallar"},
+    ],
+  },
+  {
+    titulo: "Acompañamientos",
+    productos: [
+      {img: "/img/hamburguesa.png", nombre: "triple con queso", precio: 5500, descripcion: "Tres medallones de carne 100% vacuna, queso derretido, mostaza, kétchup y cebolla triturada, es algo que nunca puede fallar"},
+    ]
+  },
+  {
+    titulo: "Bebidas",
+    productos: [
+      {img: "/img/hamburguesa.png", nombre: "triple con queso", precio: 5500, descripcion: "Tres medallones de carne 100% vacuna, queso derretido, mostaza, kétchup y cebolla triturada, es algo que nunca puede fallar"},
+    ]
+  },
+  {
+    titulo: "Postres",
+    productos: [
+      {img: "/img/hamburguesa.png", nombre: "triple con queso", precio: 5500, descripcion: "Tres medallones de carne 100% vacuna, queso derretido, mostaza, kétchup y cebolla triturada, es algo que nunca puede fallar"},
+    ]
+  },
+  {
+    titulo: "Desayunos",
+    productos: [
+      {img: "/img/hamburguesa.png", nombre: "triple con queso", precio: 5500, descripcion: "Tres medallones de carne 100% vacuna, queso derretido, mostaza, kétchup y cebolla triturada, es algo que nunca puede fallar"},
+    ]
+  },
+]
+
+const contenedor = document.getElementById("menu");
+
+menu.forEach(seccion => {
+  const seccionDiv = document.createElement("section");
+  seccionDiv.className = "container";
+
+  const h1 = document.createElement("h1");
+  h1.textContent = seccion.titulo;
+
+  contenedor.appendChild(h1);
+  contenedor.appendChild(seccionDiv);
+
+  seccion.productos.forEach(prod => {
+    const article = document.createElement("article");
+    article.className = "product-container";
+
+    article.innerHTML = `
+      <img src="${prod.img}" alt="imagen de ${prod.nombre}">
+      <div class="product-details">
+          <h2>${prod.nombre}</h2>
+          <h3>$${prod.precio}</h3>
+          <p>${prod.descripcion}</p>
+      </div>
+      <div class="input-cantidad">
+          <div>
+              <button class="btn-restar">−</button>
+              <input type="number" class="campo-cantidad" min="1" max="20" value="0" />
+              <button class="btn-sumar">+</button>
+          </div>
+          <p class='total'>Total a pagar: $0</p>
+      </div>
+      <hr>
+      <div class="btns-compra">
+          <a href='postcompra.html'>Pagar ahora</a>
+          <a href='#'>Añadir al carrito</a>
+      </div>
+    `;
+
+    seccionDiv.appendChild(article);
+  });
+});
+
+
+
+/* LOGICA DE ACUMULACION DE PRECIO */
 document.addEventListener("DOMContentLoaded", () => {
   const contenedores = document.querySelectorAll(".input-cantidad");
 
