59bc3a73a41a31584e1baaf53bc56c67b98b3533
Langenheim Geronimo Valentin <langenheimgeronimo8@gmail.com>
2025-06-30 22:41:12 -0300

    Corrigiendo detalles de resenia

--- js/resenias.js
+++ js/resenias.js
@@ -1,6 +1,7 @@
 const resenias = [
   {
-    imagen: "imagen del producto",
+    imagen: "/img/big-mac.png",
+    titulo: 'Big Mac',
     comentarios: [
       {
         nombre: "Emanuel San Roman", 
@@ -12,30 +13,157 @@ const resenias = [
         puntuacion: 4,
         comentario: 'muy buena.'
       },
+      {
+        nombre: "Ciro Salaberry", 
+        puntuacion: 5,
+        comentario: '.'
+      },
+      {
+        nombre: "Emanuel San Roman", 
+        puntuacion: 1,
+        comentario: 'muy fea.'
+      },
+      {
+        nombre: "Geronimo Langenheim", 
+        puntuacion: 4,
+        comentario: 'muy buena.'
+      },
+      {
+        nombre: "Ciro Salaberry", 
+        puntuacion: 5,
+        comentario: '.'
+      },
+      {
+        nombre: "Ciro Salaberry", 
+        puntuacion: 5,
+        comentario: '.'
+      },
     ]
   },
+  {
+    imagen: "/img/kitKat.png",
+    titulo: 'McFlurry Kit Kat',
+    comentarios: [
+      {
+        nombre: "Emanuel San Roman", 
+        puntuacion: 1,
+        comentario: 'muy fea.'
+      },
+      {
+        nombre: "Geronimo Langenheim", 
+        puntuacion: 4,
+        comentario: 'muy buena.'
+      },
+      {
+        nombre: "Ciro Salaberry", 
+        puntuacion: 5,
+        comentario: '.'
+      },
+      {
+        nombre: "Emanuel San Roman", 
+        puntuacion: 1,
+        comentario: 'muy fea.'
+      },
+      {
+        nombre: "Geronimo Langenheim", 
+        puntuacion: 4,
+        comentario: 'muy buena.'
+      },
+      {
+        nombre: "Ciro Salaberry", 
+        puntuacion: 5,
+        comentario: '.'
+      },
+      {
+        nombre: "Ciro Salaberry", 
+        puntuacion: 5,
+        comentario: '.'
+      },
+    ]
+  },
+  
 ]
 
+
+
 function cargarResenias(){
-  comentarios.forEach(categoria => {
+  
+  resenias.forEach(resenia => {
+    const section = document.createElement("section");
+    const divContenedor = document.createElement("div");
+    const image = document.createElement('img');
+    const title = document.createElement('h2');
+    const divTitulo = document.createElement('div');
     
-    const articulo = document.createElement("section");
-    categoriaDiv.className = "contenedorProductos";
-    main.appendChild(categoriaDiv);
+    image.src = resenia.imagen;
+    divContenedor.className = "primer-contenedor";
+    divTitulo.className = 'divTitulo';
+    title.innerHTML = resenia.titulo;
 
+    divTitulo.appendChild(title);
+    divTitulo.appendChild(image);
+    section.appendChild(divTitulo);
+    section.appendChild(divContenedor);
+    if(resenias.indexOf(resenia) != resenias.length-1) {
+      section.appendChild(document.createElement('hr'));
+    }
+    document.querySelector('main').appendChild(section);
 
-    categoria.productos.forEach(producto => {
+    resenia.comentarios.forEach(comentario => {
 
-      const carta = crearCarta(producto);
+        const resenia = crearResenia(comentario); 
+        divContenedor.appendChild(resenia);
 
-      if(!window.location.href.includes('inicio.html')) {
-          categoriaDiv.appendChild(carta);
-      } 
-      else{
-        if(producto.promocion){
-            categoriaDiv.appendChild(carta);
-        }
-      }
     });
   });
 }
+
+cargarResenias();
+
+function crearResenia(comentario){
+  const contenedor = document.createElement("article");
+  contenedor.className = "contenedor-opinion";
+  
+  let puntuacion = "";
+  puntuacion += `<img src="/img/logo-puntuacion.png">`.repeat(comentario.puntuacion) + `<img src="/img/logo-gris.png">`.repeat(5 - comentario.puntuacion);
+
+
+  contenedor.innerHTML = `
+                <div>
+                    <h4>${comentario.nombre}</h4>
+                    <div>
+                        ${puntuacion}
+                    </div>
+                </div>
+
+                <p>${comentario.comentario}</p>
+            `;
+  return contenedor;
+}
+
+
+/*
+function crearResenia(comentario){
+  const contenedor = document.createElement("article");
+  contenedor.className = "contenedor-opinion";
+
+  // generar las estrellas como imágenes
+  let estrellasHTML = "";
+  for(let i = 0; i < comentario.puntuacion; i++) {
+    estrellasHTML += `<img src="/img/logo.png" alt="estrella" class="estrella">`;
+  }
+
+  contenedor.innerHTML = `
+    <div>
+      <h4>${comentario.nombre}</h4>
+      <div class="estrellas">
+        ${estrellasHTML}
+      </div>
+    </div>
+    <p>${comentario.comentario}</p>
+  `;
+
+  return contenedor;
+}
+
+*/
